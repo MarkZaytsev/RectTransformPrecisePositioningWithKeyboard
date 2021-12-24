b73ebae23827fc6431e8278bfecfc8f59b32fa28
MarkZaytsev <markzaytsevdev@gmail.com>
2021-12-24 17:06:36 +0700

    Initial setup

---
+++ MoveRectTransformWithArrows.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+/// Script allows to position canvas objects more precisely.
+/// You need to select the object and set focus to the scene view or inpsector for script to process events.
+[CanEditMultipleObjects]
+[CustomEditor(typeof(RectTransform), true)]
+public class MoveRectTransformWithArrows : UnityEditor.Editor
+{
+    private const float Shift = 0.1f;
+    private const string UndoName = "MoveRectTransform";
+
+    private RectTransform _targetTransf;
+    private UnityEditor.Editor _editorInstance;
+
+    private void OnEnable()
+    {
+        var ass = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var rtEditor = ass.GetType("UnityEditor.RectTransformEditor");
+        _editorInstance = CreateEditor(target, rtEditor);
+    }
+
+    //Event.Use() should not be called from OnInspectorGUI
+    public override void OnInspectorGUI()
+    {
+        _editorInstance.OnInspectorGUI();
+        HandleMovement();
+    }
+
+    public void OnSceneGUI()
+    {
+        WorkaroundRectTransformInheritance();
+
+        if (HandleMovement())
+            Event.current.Use();
+    }
+
+    //https://forum.unity.com/threads/recttransform-custom-editor-ontop-of-unity-recttransform-custom-editor.455925/
+    private void WorkaroundRectTransformInheritance()
+    {
+        var onSceneGUIMethod = _editorInstance.GetType()
+            .GetMethod("OnSceneGUI", BindingFlags.NonPublic | BindingFlags.Instance);
+        onSceneGUIMethod?.Invoke(_editorInstance, null);
+    }
+
+    private bool HandleMovement()
+    {
+        _targetTransf = target as RectTransform;
+
+        var e = Event.current;
+        switch (e.keyCode)
+        {
+            case KeyCode.UpArrow:
+                Undo.RecordObject(_targetTransf, UndoName);
+                MoveRect(_targetTransf, 0f, Shift);
+                return true;
+            case KeyCode.DownArrow:
+                Undo.RecordObject(_targetTransf, UndoName);
+                MoveRect(_targetTransf, 0f, -Shift);
+                return true;
+            case KeyCode.LeftArrow:
+                Undo.RecordObject(_targetTransf, UndoName);
+                MoveRect(_targetTransf, -Shift, 0f);
+                return true;
+            case KeyCode.RightArrow:
+                Undo.RecordObject(_targetTransf, UndoName);
+                MoveRect(_targetTransf, Shift, 0f);
+                return true;
+        }
+
+        return false;
+    }
+
+    private static void MoveRect(Transform rect, float x, float y)
+    {
+        rect.localPosition += new Vector3(x, y);
+        EditorUtility.SetDirty(rect);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-RectTransformPrecisePositioningWithKeyboard
+# RectTransformPrecisePositioningWithKeyboard
+This is a script for Unity Editor. 
+The script allows to precisely position a selected object with RectTransform component in the scene view using keyboard's arrow keys.
+I find it useful option for 2d mode workflow with Canvas objects during layouting according to design. But might be worth it to extend the feature for the Transform conmonent to work with 2d sprites.
+
+- Put the script to Editor folder
+- Select a game object in the scene
+- Script will process events only if game object is selected and SceneView or Inspector is focused. E.g. you might want to click on object first, and then on inspector or scene view.
